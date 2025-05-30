a20c1b8c20c7322e971eea1f9090e4ae6a9c6008
aswanthmpt <aswanthmpt@gmail.com>
2025-05-30 14:27:41 +0530

    Readyshkf

--- portfolio/src/pages/Contact/Contact.jsx
+++ portfolio/src/pages/Contact/Contact.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Contact.css'
 import nv from '../../assets/gps-navigation.png'
 import ml from '../../assets/email.png'
 import ph from '../../assets/phone-call.png'
 const Contact = () => {
+   const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    mobile: '',
+    message: ''
+  });
+   const handleChange = (e) => {
+    setFormData({ 
+      ...formData, 
+      [e.target.name]: e.target.value 
+    });
+  };
+   const sendToWhatsApp = (e) => {
+    e.preventDefault();
+    
+    const { name, email, mobile, message } = formData;
+
+    const whatsappMessage = `Name: ${name}%0AEmail: ${email}%0AMobile: ${mobile}%0AMessage: ${message}`;
+    
+    const whatsappURL = `https://wa.me/9048642860?text=${whatsappMessage}`;
+    
+    window.open(whatsappURL, '_blank');
+  };
   return (
     <div className='contactme'>
       <div className="row">
@@ -22,12 +45,32 @@ const Contact = () => {
         </div>
         <div className="col-lg-6 col-md-6 col-sm-6">
           <div className="contactme-form">
-            <form action="">
-              <input type="text" placeholder='name' />
-              <input type="text" placeholder='Email' />
-              <input type="text" placeholder='Mobile no' />
-              <textarea type="text" rows={4} placeholder=' Message' />
-              <button>Send</button>
+            <form onSubmit={sendToWhatsApp}>
+              <input  type="text"
+                name="name"
+                placeholder='Name'
+                value={formData.name}
+                onChange={handleChange}
+                required/>
+              <input  type="email"
+                name="email"
+                placeholder='Email'
+                value={formData.email}
+                onChange={handleChange}
+                required />
+              <input  type="text"
+                name="mobile"
+                placeholder='Mobile No'
+                value={formData.mobile}
+                onChange={handleChange}
+                required/>
+              <textarea  name="message"
+                rows={4}
+                placeholder='Message'
+                value={formData.message}
+                onChange={handleChange}
+                required />
+              <button type="submit">Send</button>
             </form>
           </div>
         </div>
--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -7,25 +7,31 @@ import img1 from '../../assets/image1.jpg'
 const Home = () => {
   return (
     <div className='container-fluid main '>
-       <div className="container intro ">
-         <div className="row">
-              <div className="col-lg-6 col-md-6 col-sm-6 headding-col">
-                <div className="headding">
-            <h1>Hi,I am <span>Aswanth</span></h1>
-            <h1>Fullstack Webdeveloper</h1>
-            <div className="contactme_btn">
-              <button>Hire Me</button>
-              <button>Download CV</button>
+      <div className="container intro ">
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-6 headding-col">
+            <div className="headding">
+              <h1>Hi,I am <span>Aswanth</span></h1>
+              <h1>Fullstack Webdeveloper</h1>
+              <div className="contactme_btn">
+                <a
+                  href="https://wa.me/919048642860?text=Hi%20Aswanth%2C%20I%20am%20interested%20in%20hiring%20you!"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button>Hire Me</button>
+                </a>
+                <a href="/Aswanthmpt_cv_.pdf" download><button>Download CV</button></a>
+              </div>
             </div>
- </div>
-            </div>
-            <div className="col-lg-6 col-md-6 col-sm-6 image-col">
-                <img src={img1} alt="" />
-            </div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-6 image-col">
+            <img src={img1} alt="" />
+          </div>
         </div>
-       </div>
-       
-      
+      </div>
+
+
     </div>
   )
 }
